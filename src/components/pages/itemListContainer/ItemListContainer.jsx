b28e72491aefc0075cfa02ecf0ce445ab7ad0b73
Charles Bemón <charlesbemon@gmail.com>
2024-05-09 00:05:00 -0300

    skeleton on

--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -2,13 +2,12 @@ import { useState, useEffect } from "react";
 import { products } from "../../../ProductsMock.js";
 import { useParams } from "react-router-dom";
 import ItemList from "./ItemList";
-import { PiArrowSquareOutLight } from "react-icons/pi";
-
+import { Skeleton } from "@mui/material";
 
 const ItemListContainer = () => {
   const { type } = useParams();
   const [items, setItems] = useState([]);
-  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     let filteredProducts = [];
@@ -25,13 +24,61 @@ const ItemListContainer = () => {
       filteredProducts = products;
     }
 
-    console.log(filteredProducts);
-
-    setItems(filteredProducts);
+    setTimeout(() => {
+      setItems(filteredProducts);
+      setLoading(false);
+    }, 200);
   }, [type]);
 
-  return <ItemList items={items} error={error} />;
+  return (
+    <>
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "30px",
+          }}
+        >
+          {[...Array(3)].map((_, rowIndex) => (
+            <div
+              key={rowIndex}
+              style={{
+                display: "flex",
+                gap: "20px",
+                marginBottom: "20px",
+              }}
+            >
+              {[...Array(4)].map((_, colIndex) => (
+                <div key={colIndex}>
+                  <Skeleton variant="rectangular" sx={{ fontSize: "1rem" }} height={200} width={280} />
+                  <Skeleton variant="text" sx={{ fontSize: "1rem" }} height={40} width={280} />
+                  <Skeleton variant="text" sx={{ fontSize: "0.5rem" }} height={20} width={100} />
+                </div>
+              ))}
+            </div>
+          ))}
+          <div
+            style={{
+              display: "flex",
+              gap: "20px",
+            }}
+          >
+            {[...Array(items.length % 4)].map((_, colIndex) => (
+              <div key={colIndex}>
+                <Skeleton variant="rectangular" sx={{ fontSize: "1rem" }} height={200} width={280} />
+                <Skeleton variant="text" sx={{ fontSize: "1rem" }} height={40} width={280} />
+                <Skeleton variant="text" sx={{ fontSize: "0.5rem" }} height={20} width={100} />
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <ItemList items={items} />
+      )}
+    </>
+  );
 };
 
 export default ItemListContainer;
-
